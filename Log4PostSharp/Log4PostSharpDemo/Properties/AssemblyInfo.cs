04e6a997b1602f7cd447cc0749d849f7c9289a0b
herrmic <herrmic@c6bede1d-6443-0410-8cde-f9b26897b465>
2008-06-02 20:00:55 +0000

    Added demo solution.

---
+++ Log4PostSharp/Log4PostSharpDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+using log4net.Config;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Log4PostSharpDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michal Dabrowski")]
+[assembly: AssemblyProduct("Log4PostSharpDemo")]
+[assembly: AssemblyCopyright("Copyright © Michal Dabrowski 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd4c8c6e-dea0-4ec4-87f6-b0a997e8393d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Configure log4net.
+[assembly: XmlConfigurator(Watch = true)]
