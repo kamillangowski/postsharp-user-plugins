74a7a96065ab136769bb1ba9cbca6b24e48f2f38
gael.fraiteur@gmail.com <gael.fraiteur@gmail.com@c6bede1d-6443-0410-8cde-f9b26897b465>
2008-11-08 14:23:03 +0000

    DesignByContract: DemoDbC

---
+++ trunk/1.5/DesignByContract/DemoDbC/BuggyAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoDbC
+{
+    class BuggyAccount : IAccount
+    {
+        public void Debit(decimal amount)
+        {
+            
+        }
+
+        public void Credit(decimal amount)
+        {
+            
+        }
+
+        public decimal Balance
+        {
+            get { return 0; }
+        }
+    }
+}
--- trunk/1.5/DesignByContract/DemoDbC/IAccount.cs
+++ trunk/1.5/DesignByContract/DemoDbC/IAccount.cs
@@ -5,10 +5,10 @@ namespace DemoDbC
 {
     public interface IAccount
     {
-        [Dbc("amount>0")]
+        [Dbc("amount>0", "Balance<[old]Balance")]
         void Debit(decimal amount);
 
-        [Dbc("amount>0")]
+        [Dbc("amount>0", "Balance>[old]Balance")]
         void Credit(decimal amount);
 
         decimal Balance { get; }
