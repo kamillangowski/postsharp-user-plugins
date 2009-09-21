992cb4824b6670f88df52047fe0d4f285c75aa76
jay.cincotta@esymmetrix.com <jay.cincotta@esymmetrix.com@c6bede1d-6443-0410-8cde-f9b26897b465>
2009-09-21 05:09:29 +0000

    Baseline checkin of rGSharp demo program

---
+++ trunk/1.5/GSharp/Demo/Program.cs
@@ -0,0 +1,65 @@
+﻿#region Released to Public Domain by eSymmetrix, Inc.
+
+/********************************************************************************
+ *   This file is sample code demonstrating Gibraltar integration with PostSharp
+ *   
+ *   This sample is free software: you have an unlimited rights to
+ *   redistribute it and/or modify it.
+ *   
+ *   This sample is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   
+ *******************************************************************************/
+
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+#endregion
+
+namespace GSharpDemo
+{
+    /// <summary>
+    /// A sample application using dynamic binding to Gibraltar.Agent.dll via app.config.
+    /// </summary>
+    /// <remarks><para>
+    /// This example is compiled without direct reference to Gibraltar, only adding Gibraltar's TraceListener
+    /// (Gibraltar.Agent.LogListener) in the trace section of the system.diagnostics group in the app.config file to
+    /// connect the Agent through the use of built-in Trace logging.  This allows the Gibraltar Agent to be attached to
+    /// an application which is already using Trace or other logging systems without recompiling your application.
+    /// </para>
+    /// <para>
+    /// This example shows a typical winforms Program.Main() with a few recommended calls to make sure the
+    /// Gibraltar Agent can perform at its best.
+    /// </para>
+    /// <para>
+    /// Also see the static binding sample application as an alternative approach showing some of the features of the
+    /// Gibraltar API.</para></remarks>
+    internal static class Program
+    {
+        public static TimeSpanCollection TimeSpans = new TimeSpanCollection();
+
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Fire off a message so Trace will scan the app.config (see also) and add Gibraltar's TraceListener.
+            // This ensures that the Gibraltar Agent is loaded and activated, scanning its own part of app.config.
+            // Without a message at this point, the Agent would only get loaded when a message is logged some time later,
+            // and some of the Agent's automatic features would not be able to function at their best for you.
+
+            Trace.TraceInformation("Application starting.");
+
+            // Nothing else is needed to activate exception handling with Gibraltar on most winforms apps, 
+            // as soon as the first line of logging returns above it's active.
+
+            Application.Run(new MainApp());
+
+            Trace.TraceInformation("Application exiting."); // Just for completeness.
+            Trace.Close(); // It's always a good idea to call Trace.Close when you're exiting the application.  This ensures that every listener has the chance to shutdown cleanly.
+        }
+    }
+}
---
+++ trunk/1.5/GSharp/Demo/WorkerUI.cs
@@ -0,0 +1,160 @@
+﻿#region Released to Public Domain by eSymmetrix, Inc.
+
+/********************************************************************************
+ *   This file is sample code demonstrating Gibraltar integration with PostSharp
+ *   
+ *   This sample is free software: you have an unlimited rights to
+ *   redistribute it and/or modify it.
+ *   
+ *   This sample is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   
+ *******************************************************************************/
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+#endregion
+
+namespace GSharpDemo
+{
+    public partial class WorkerUI : UserControl
+    {
+        private bool m_KeepRunning;
+        private Thread m_WorkerThread;
+        private int m_WorkerThreadSpeed;
+        private int m_WorkerWaitTime;
+        private int m_cycleCount;
+
+        private void ComputeWaitTime()
+        {
+            // workerSpeed comes from m_workerThreadSpeed, which comes from trackBarSpeed, which ranges from 0 to 20
+            // This results in a sleep of 10..110ms per iteration resulting in max cylce time of about 2.5 seconds
+            m_WorkerWaitTime = 10 + 5 *(20 - m_WorkerThreadSpeed);
+        }
+
+        public WorkerUI()
+        {
+            InitializeComponent();
+            trackbarSpeed.Minimum = 0;
+            trackbarSpeed.Maximum = 20;
+            trackbarSpeed.Value = 20;
+            m_WorkerThreadSpeed = trackbarSpeed.Value;
+            ComputeWaitTime();
+        }
+
+        private void trackbarSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            m_WorkerThreadSpeed = trackbarSpeed.Value;
+            ComputeWaitTime();
+        }
+
+
+        private void HighlightCycleCount()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(HighlightCycleCount));
+            }
+            else
+            {
+                lblCycleCount.Text = m_cycleCount.ToString();
+                verticalProgressBar1.ProgressInPercentage = 0;
+                verticalProgressBar2.ProgressInPercentage = 0;
+                verticalProgressBar3.ProgressInPercentage = 0;
+                lblCycleCount.ForeColor = Color.Red;
+                BackColor = Color.LightYellow;
+
+            }
+        }
+
+        private void UnhighlightCycleCount()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(UnhighlightCycleCount));
+            }
+            else
+            {
+                lblCycleCount.ForeColor = SystemColors.ControlText;
+                BackColor = Color.White;
+
+            }
+        }
+
+        public void Start()
+        {
+            m_KeepRunning = true;
+            m_WorkerThread = new Thread(WorkerThread) {IsBackground = true};
+            m_WorkerThread.Start();
+        }
+
+        private void WorkerThread()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int progress1 = 0;
+            int progress2 = 0;
+            int progress3 = 0;
+
+            while (m_KeepRunning)
+            {
+                progress1 += 20;
+
+                if (progress1 > 100)
+                {
+                    progress1 = 0;
+                    progress2 += 20;
+                    Trace.WriteLine("First bar has spilled over.");
+                }
+
+                if (progress2 > 100)
+                {
+                    progress2 = 0;
+                    progress3 += 20;
+                    Trace.TraceInformation("Second bar has spilled over.");
+                }
+
+                if (progress3 > 100)
+                {
+                    progress3 = 0;
+                    m_cycleCount++;
+                    stopWatch.Stop();
+                    Trace.TraceWarning("Third bar has spilled over. Cycle-time: "
+                                       + stopWatch.Elapsed.TotalSeconds.ToString("F2"));
+
+                    HighlightCycleCount();
+                    Thread.Sleep(500);
+                    UnhighlightCycleCount();
+
+                    Program.TimeSpans.Add(stopWatch.Elapsed);
+                    stopWatch.Reset();
+                    stopWatch.Start();
+                }
+
+                verticalProgressBar1.ProgressInPercentage = progress1;
+                verticalProgressBar2.ProgressInPercentage = progress2;
+                verticalProgressBar3.ProgressInPercentage = progress3;
+
+                Thread.Sleep(m_WorkerWaitTime);
+            }
+        }
+
+        public void Stop()
+        {
+            m_KeepRunning = false; // Stop worker thread
+            if (m_WorkerThread != null)
+                m_WorkerThread.Join(500);
+        }
+
+        private void WorkerUI_Resize(object sender, EventArgs e)
+        {
+            Width = 140;
+        }
+    }
+}
