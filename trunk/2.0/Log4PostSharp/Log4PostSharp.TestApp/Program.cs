9b4c3d1bf4cd890b3cca08013a77b10624d6cf91
k.p.novoselov <k.p.novoselov@c6bede1d-6443-0410-8cde-f9b26897b465>
2010-03-18 16:33:30 +0000

    Log4PostSharpDemo and Log4PostSharp.TestApp added to solution

---
+++ trunk/2.0/Log4PostSharp/Log4PostSharp.TestApp/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Log4PostSharp.TestApp
+{
+    public class Program
+    {
+        public static void MethodWithNoReturnValue()
+        {
+        }
+
+        [Log(EntryLevel = LogLevel.Fatal, EntryText = "Calling {@someId}.")]
+        [Log(ExitLevel = LogLevel.Fatal, ExitText = "Return value is: '{returnvalue}'.")]
+        public static void MethodWithNoReturnValue(Guid someId)
+        {
+        }
+
+        [Log(EntryLevel = LogLevel.Debug, EntryText = "Test for '{@i1}', '{@i2}', '{@r1}', '{@r2}'.")]
+        [Log(ExitLevel = LogLevel.Fatal, ExitText = "Was called with params: {paramvalues}.")]
+        [Log(ExitLevel = LogLevel.Fatal, ExitText = "Return value is: '{returnvalue}'.")]
+        public static int MultipleArgsMethod(int i1, string i2, ref int r1, ref object r2, out Guid o1, out string o2)
+        {
+            o1 = Guid.NewGuid();
+            o2 = "X";
+            return i1 + 50;
+        }
+
+        [Log(LogLevel.Info, "Test message.")]
+        public static Guid MethodWithGuidReturnValue()
+        {
+            return Guid.NewGuid(); 
+        }
+
+        [Log(EntryLevel = LogLevel.Info, EntryText = "Doing stuff.", ExitLevel = LogLevel.None, ExceptionLevel = LogLevel.None)]
+        public static int MethodWithIntReturnValue()
+        {
+            return int.MinValue;
+        }
+
+        public static void Main(string[] args)
+        {
+            string.Format(CultureInfo.InvariantCulture, "{0}{1}", args, 5);
+
+            MethodWithNoReturnValue();
+            MethodWithNoReturnValue(Guid.NewGuid());
+            int r1 = 10;
+            object r2 = "X";
+            Guid o1;
+            string o2;
+            MultipleArgsMethod(1, "A", ref r1, ref r2, out o1, out o2);
+
+            MethodWithGuidReturnValue();
+            MethodWithIntReturnValue();
+
+            new AnotherSubprogram<int>().Act<string>();
+        }
+
+        public class SubclassWithCompilerGeneratedCode
+        {
+            public IEnumerable<string> MethodThatAddsCompilerGeneratedCode()
+            {
+                yield return "A";
+                yield return "B";
+            }
+        }
+
+        public class Subprogram
+        {
+            [Log]
+            public Subprogram()
+            {
+            }
+        }
+
+        public class AnotherSubprogram<T>
+        {
+            static readonly object x = new object();
+
+            public T DoStuff(T t)
+            {
+                return default(T);
+            }
+
+            public U Act<U>()
+            {
+                x.ToString();
+
+                return default(U);
+            }
+
+            public W Act<U, V, W>(U u, V v)
+            {
+                return default(W);
+            }
+        }
+
+        public interface ITest
+        {
+            void DoTest();
+        }
+    }
+}
---
+++ trunk/2.0/Log4PostSharp/Log4PostSharpDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+
+Copyright (c) 2008, Michal Dabrowski
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the Michal Dabrowski nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+using System;
+using System.Globalization;
+
+using Log4PostSharp;
+
+namespace Log4PostSharpDemo
+{
+    public class Program
+    {
+        [Log(EntryLevel = LogLevel.Debug, EntryText = "Adding {@i1} to {@i2}.", ExitLevel = LogLevel.Debug, ExitText = "Result of addition is {returnvalue}.")]
+        private static int Add(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+
+        [Log(EntryLevel = LogLevel.Debug, ExitLevel = LogLevel.Debug, ExceptionLevel = LogLevel.Fatal)]
+        private static int ReadNumber()
+        {
+            // Display prompt.
+            Console.Write("Please enter a number: ");
+
+            // Read the line from the console.
+            string line = Console.ReadLine();
+
+            // Convert the data into the integer.
+            return int.Parse(line, CultureInfo.CurrentCulture);
+        }
+
+        public static void Main(string[] args)
+        {
+            // Get operands.
+            int i1 = ReadNumber();
+            int i2 = ReadNumber();
+
+            // Calculate the sum.
+            int sum = Add(i1, i2);
+
+            // Print the result.
+            Console.WriteLine("Result is {0}.", sum);
+        }
+    }
+}
